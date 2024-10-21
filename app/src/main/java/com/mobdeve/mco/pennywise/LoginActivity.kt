f4515649b8e68e1e97757fd9dea4543734bd2d47
GeneCedricAlejo <97246520+CedricAlejo21@users.noreply.github.com>
2024-10-21 14:22:42 +0800

    Initial commit

---
+++ app/src/main/java/com/mobdeve/mco/pennywise/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.mco.pennywise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
