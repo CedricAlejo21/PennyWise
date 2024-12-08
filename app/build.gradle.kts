4dc20f900e08c952c435ca2e0ec3361c605e1f7e
GeneCedricAlejo <97246520+CedricAlejo21@users.noreply.github.com>
2024-12-09 03:13:45 +0800

    Removed Profile Page to scale down, moved some of its buttons to dashboard, added red button, added chart placeholder, added chart dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,5 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.2.3")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
